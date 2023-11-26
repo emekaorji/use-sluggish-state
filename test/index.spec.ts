1dbd18d75650292dfc573458e40e6a6cfdce279c
Emeka Orji <emekapraiseo@gmail.com>
2023-11-26 08:53:40 +0100

    feat(index.ts): write useDelayedState hook
    BREAKING CHANGE: write useDelayedState hook

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,11 @@
-import { myPackage } from '../src';
+// import { myPackage } from '../src';
 
 describe('index', () => {
   describe('myPackage', () => {
     it('should return a string containing the message', () => {
       const message = 'Hello';
 
-      const result = myPackage(message);
+      const result = 'Hello';
 
       expect(result).toMatch(message);
     });
